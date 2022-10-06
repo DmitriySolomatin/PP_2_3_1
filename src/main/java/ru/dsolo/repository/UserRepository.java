53b36f5ae99d95e5ae7a6a8322784d443a739431
DmitriySolomatin <solomatin@megavega.ru>
2022-10-06 22:20:41 +0400

    add Repository

--- src/main/java/ru/dsolo/repository/UserRepository.java
+++ src/main/java/ru/dsolo/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package ru.dsolo.repository;public interface UserRepository {
+package ru.dsolo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.dsolo.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/ru/dsolo/service/UserServiceImp.java
+++ src/main/java/ru/dsolo/service/UserServiceImp.java
@@ -1,42 +1,46 @@
 package ru.dsolo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.dsolo.dao.UserDao;
 import ru.dsolo.model.User;
+import ru.dsolo.repository.UserRepository;
 
 import java.util.List;
 
 @Service
 public class UserServiceImp implements UserService {
-    private final UserDao userDao;
+    private final UserRepository userRepository;
+
 
     @Autowired
-    public UserServiceImp(UserDao userDao) {
-        this.userDao = userDao;
+    public UserServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public User getUserById(long id) {
-        return userDao.getUserById(id);
+        return userRepository.getReferenceById(id);
     }
 
     public List<User> getUserList(int count) {
-        return userDao.getUserList(count);
+        return userRepository.findAll(PageRequest.of(0,count)).stream().toList();
     }
 
     @Transactional
     public void deleteUser(long id) {
-        userDao.deleteUser(id);
+        userRepository.delete(getUserById(id));
     }
 
     @Transactional
     public void addUser(User user) {
-        userDao.addUser(user);
+        userRepository.save(user);
     }
 
     @Transactional
     public void updateUser(User user) {
-        userDao.updateUser(user);
+        userRepository.save(user);
     }
 }
